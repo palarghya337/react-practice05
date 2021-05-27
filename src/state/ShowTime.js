afc0ece55c67321cb6be05199c2558e7bd7f5341
palarghya337 <palarghya337@gmail.com>
2021-05-27 12:18:11 +0530

    Added Show Time function which will print time every seond.

---
+++ src/state/ShowTime.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class ShowTime extends Component {
+  constructor() {
+    super();
+    this.state = { date: new Date() };
+  }
+
+  componentDidMount() {
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return <div>{this.state.date.toLocaleTimeString()}</div>;
+  }
+}
+export default ShowTime;
--- src/state/StateExample.js
+++ src/state/StateExample.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import BasicStateExample from './BasicStateExample';
 import SetStateExample from './SetStateExample';
+import ShowTime from './ShowTime';
 
 function StateExample() {
   return (
     <div>
       <BasicStateExample />
       <SetStateExample />
+      <ShowTime />
     </div>
   );
 }
